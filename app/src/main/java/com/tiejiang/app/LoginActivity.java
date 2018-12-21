cf000b37ac78b60cff6c3f1cdf7b9398f76e7c5f
hutao <643759269@qq.com>
2018-12-21 10:04:27 +0800

    融云demo1.0

---
+++ app/src/main/java/com/tiejiang/app/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.tiejiang.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+        String token = "FKcpIMG98TALWQvMjn+kuSRQyT9m+MJ2vukyxULq+RVxD4bXLxOYArfrQEIQdQTGGNcpLxv8ZE5Tg7s+o5iTcw==";
+        connect(token);
+
+
+    }
+
+    /**
+     * <p>连接服务器，在整个应用程序全局，只需要调用一次，需在 {@link #init(Context)} 之后调用。</p>
+     * <p>如果调用此接口遇到连接失败，SDK 会自动启动重连机制进行最多10次重连，分别是1, 2, 4, 8, 16, 32, 64, 128, 256, 512秒后。
+     * 在这之后如果仍没有连接成功，还会在当检测到设备网络状态变化时再次进行重连。</p>
+     *
+     * @param token    从服务端获取的用户身份令牌（Token）。
+     * @param callback 连接回调。
+     * @return RongIM  客户端核心类的实例。
+     */
+    private void connect(String token) {
+
+        if (getApplicationInfo().packageName.equals(MyApplication.getCurProcessName(getApplicationContext()))) {
+
+            RongIM.connect(token, new RongIMClient.ConnectCallback() {
+
+                /**
+                 * Token 错误。可以从下面两点检查 1.  Token 是否过期，如果过期您需要向 App Server 重新请求一个新的 Token
+                 *                  2.  token 对应的 appKey 和工程里设置的 appKey 是否一致
+                 */
+                @Override
+                public void onTokenIncorrect() {
+
+                }
+
+                /**
+                 * 连接融云成功
+                 * @param userid 当前 token 对应的用户 id
+                 */
+                @Override
+                public void onSuccess(String userid) {
+                    Log.d("LoginActivity", "--onSuccess" + userid);
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                }
+
+                /**
+                 * 连接融云失败
+                 * @param errorCode 错误码，可到官网 查看错误码对应的注释
+                 */
+                @Override
+                public void onError(RongIMClient.ErrorCode errorCode) {
+
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/tiejiang/app/MyApplication.java
@@ -0,0 +1,32 @@
+package com.tiejiang.app;
+
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+
+import io.rong.imkit.RongIM;
+
+/**
+ * Created by hutao on 2018-12-21.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RongIM.init(this);
+    }
+
+
+    public static String getCurProcessName(Context context) {
+        int pid = android.os.Process.myPid();
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo appProcess : activityManager.getRunningAppProcesses()) {
+            if (appProcess.pid == pid) {
+                return appProcess.processName;
+            }
+        }
+        return null;
+    }
+}
